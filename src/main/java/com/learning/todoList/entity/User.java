834b82f39bd07f291b9a510fe674a0c2a13c3408
async0044 <async0044@gmail.com>
2025-06-30 07:35:00 +0700

    Begin

---
+++ src/main/java/com/learning/todoList/entity/User.java
@@ -0,0 +1,62 @@
+package com.learning.todoList.entity;
+
+import jakarta.persistence.*;
+import org.springframework.stereotype.Repository;
+
+@Entity
+@Table(name="todoUser")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String status;
+
+    public User() {}
+
+    public User(String username, String password, String email, String status) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
